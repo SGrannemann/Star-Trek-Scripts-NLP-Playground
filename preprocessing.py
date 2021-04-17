fa7601f6508122618e9d75d701eace630b5306aa
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2021-04-17 18:34:52 +0200

    Added phrasing for bigrams.
    had a look at the words in the corpus and improved data cleaning a bit.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-all_series_lines = pd.read_json('all_scripts_raw.json')
+
 
 def remove_speakers_and_empty_lines(episode_content: str) -> str:
     """Removes superfluous empty lines and the names of the speakers from the input data:
@@ -12,23 +12,30 @@ def remove_speakers_and_empty_lines(episode_content: str) -> str:
 
     cleaned_lines = []
     for line in episode_content.split('\n'):
+        # ignore empty lines
         if line == '':
             continue
+        # the actual talking lines always contain a ':' - we will just keep the text, not the talker
+        # for this application
         if ':' in line:
             part_to_keep = line.split(':')[1]
             cleaned_lines.append(part_to_keep.strip() + ' \n')
             continue
+        # after this string there are only information about the franchise, we can leave those out.
+        if line == '<Back':
+            break
         
         
         cleaned_lines.append(line.strip() + ' \n')
     return ''.join(cleaned_lines)
 
 
-
+all_series_scripts = pd.read_json('all_scripts_raw.json')
 #  remove the names of the speakers and get rid of the empty lines
 # and I'll focus on The Next Generation Episodes for now
-tng_series_lines_cleaned = all_series_lines.TNG.map(remove_speakers_and_empty_lines)
-print(tng_series_lines_cleaned)
+tng_series_scripts_cleaned = all_series_scripts.TNG.map(remove_speakers_and_empty_lines)
+
+# after this preprocessing
 
 
 

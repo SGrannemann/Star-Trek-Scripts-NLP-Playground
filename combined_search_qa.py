ab88ebaefb99c19460b2c1615c7403d8cb4e1bce
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2021-12-04 17:58:49 +0100

    fixed bug in extraction of TNG plots. added QA functionality to retrieval script.

--- combined_search_qa.py
+++ combined_search_qa.py
@@ -1,40 +1,75 @@
-# this script combines a query classification (question yes/no) with either document retrieval or answering a question
+'''Script that sets up document stores from the files created in this project. Lets you search for relevant episodes from TNG, VOY, and DS9 with either
+a TfIdf based search or a dense vector space search using dense passage retrieval. For details please see the Haystack documentation.
+Last part is a simple question answering system. 
+'''
+
 from pathlib import Path
 from haystack.document_store import InMemoryDocumentStore
 from haystack.retriever import DensePassageRetriever
+from haystack.retriever.sparse import TfidfRetriever
 from haystack.reader import FARMReader
-#from haystack.query_classifierimport SklearnQueryClassifier # this does not work as of haystack 0.10.0
+from haystack.pipeline import ExtractiveQAPipeline
+# from haystack.query_classifierimport SklearnQueryClassifier # this does not work as of haystack 0.10.0
 
 
 # get data
 documents = []
-series_abbrev = ['ds9']#, 'voy', 'tng']
+series_abbrev = ['tng', 'voy', 'tng']
 for series in series_abbrev:
     for document_path in (Path.cwd() / Path('data') / Path('scraped') / Path(series) / Path('processed')).glob('*.txt'):
         with open(document_path, 'r', encoding='utf-8') as file:
             documents.append({'text': file.read(), 'meta': {'name': document_path.stem}})
 
-# set up document store (in memory for now) 
+# set up document store (in memory for now)
 
-doc_store = InMemoryDocumentStore(similarity='dot_product')
-doc_store.write_documents(documents)
+DPR_doc_store = InMemoryDocumentStore(similarity='dot_product')
+DPR_doc_store.write_documents(documents)
+
+
+TfIdf_doc_store = InMemoryDocumentStore()
+TfIdf_doc_store.write_documents(documents)
 
 # set up DPR retriever
-retriever = DensePassageRetriever(document_store=doc_store, query_embedding_model='facebook/dpr-question_encoder-single-nq-base',
-                                    passage_embedding_model='facebook/dpr-ctx_encoder-single-nq-base')
-# the doc store needs to know the embeddings too
-doc_store.update_embeddings(retriever)
+DPR_retriever = DensePassageRetriever(document_store=DPR_doc_store,
+                                      query_embedding_model='facebook/dpr-question_encoder-single-nq-base',
+                                      passage_embedding_model='facebook/dpr-ctx_encoder-single-nq-base')
 
-# get query
-query1 = 'Who captured Geordi?'
-query2 = 'sisko and jake baseball'
+# the doc store needs to know the embeddings too
+DPR_doc_store.update_embeddings(DPR_retriever)
+
+
+TfIdf_retriever = TfidfRetriever(TfIdf_doc_store)
+
+# some test queries
+queries = ['Geordi captured by Romulans and mindcontrolled', 'Picard becomes Borg', 'Q judges humanity']
+
+# return search results
+for query in queries:
+    results_DPR = DPR_retriever.retrieve(query=query, top_k=3)
+    results_tfidf = TfIdf_retriever.retrieve(query=query, top_k=3)
+    for result in results_DPR:
+        print(f"DPR Result for {query}:  {result.meta['name']}")
+    for result in results_tfidf:
+        print(f"TfIdf Result for {query}:  {result.meta['name']}")
+
 
 # set up query classifier # left out until the query classifiers are available
 
-# return ranked search results
 
-results = retriever.retrieve(query=query2, top_k=5)
-for result in results:
-    print(result.meta['name'])
+# set up FARMReader for question answering
+model = 'deepset/roberta-base-squad2'
+reader = FARMReader(model)
 
-# set up FARMReader for question answering in case of question query
+# use pipeline with both the DPR retriever and the TfIdf retriever.
+# TfIdf returns better search results for the queries I tested (and consequently is better at answering questions)
+pipeline_dpr = ExtractiveQAPipeline(reader, DPR_retriever)
+pipeline_tfidf = ExtractiveQAPipeline(reader, TfIdf_retriever)
+questions = ['Who captures Geordi La Forge?', 'Which tea does Picard drink?', "Who is Data's brother?", 'What is the name of Benjamin Siskos son?',
+             'Who is the captain of the USS Voyager?', "What is Worf's race?",
+             'Who is the chief engineer on the USS Enterprise?', 'What is gold pressed latinum?', 'What type of torpedos does the Enterprise have?']
+
+# simply print the 3 best answers for each question
+for question in questions:
+    predictions = pipeline_tfidf.run(query=question, params={'Retriever': {'top_k': 5}, 'Reader': {'top_k': 3}})
+    for prediction in predictions['answers']:
+        print(f"{question}: {prediction['answer']}")
--- data/extract_plots.py
+++ data/extract_plots.py
@@ -3,22 +3,22 @@ scraped Wikipedia articles and writes them to a simple text file."""
 from pathlib import Path
 import re
 import bs4
-
+from bs4.element import NavigableString
 
 
 # set the series folder here
 
 # tng folders
-FOLDER_TO_WIKIDATA = Path.cwd() / Path('data') / Path('scraped') / Path('tng')
-FOLDER_FOR_SAVING = Path.cwd() / Path('data') / Path('scraped') / Path('tng') / Path('processed')
+#FOLDER_TO_WIKIDATA = Path.cwd() / Path('data') / Path('scraped') / Path('tng')
+#FOLDER_FOR_SAVING = Path.cwd() / Path('data') / Path('scraped') / Path('tng') / Path('processed')
 
 # voy folders
 #FOLDER_TO_WIKIDATA = Path.cwd() / Path('data') / Path('scraped') / Path('voy')
 #FOLDER_FOR_SAVING = Path.cwd() / Path('data') / Path('scraped') / Path('voy') / Path('processed')
 
 # ds9 folders
-#FOLDER_TO_WIKIDATA = Path.cwd() / Path('data') / Path('scraped') / Path('ds9')
-#FOLDER_FOR_SAVING = Path.cwd() / Path('data') / Path('scraped') / Path('ds9') / Path('processed')
+FOLDER_TO_WIKIDATA = Path.cwd() / Path('data') / Path('scraped') / Path('ds9')
+FOLDER_FOR_SAVING = Path.cwd() / Path('data') / Path('scraped') / Path('ds9') / Path('processed')
 
 
 # loop over all text files, find the plot element and get all para elements
@@ -31,18 +31,18 @@ for wikipage in FOLDER_TO_WIKIDATA.glob('*.txt'):
         plot = []
         # lets find the header for plot, but we only want to get the Act wise descriptions, not the teaser
         act_headings = soup.find_all(id=re.compile('^Act'))
-        
 
-        for act in act_headings: # iterate over all acts
-            for para_element in act.parent.nextSiblingGenerator(): # this line allows us to iterate over all elements that come next/under
+        for act in act_headings:  # iterate over all acts
+            for para_element in act.parent.nextSiblingGenerator():  # this line allows us to iterate over all elements that come next/under
                 # to an act heading
-                if para_element.name in ['h3', 'h2']: # this is either the next act or we are done with the plot description
+                if para_element.name in ['h3', 'h2']:  # this is either the next act or we are done with the plot description
                     break
-                if para_element.name == 'figure': # we dont want the captions
+                if para_element.name == 'figure':  # we dont want the captions
                     continue
-                if para_element == '\n' or ' ':
+                if isinstance(para_element, NavigableString):
                     continue
                 if para_element.text:
+
                     plot.append(para_element.text)
 
         # process a bit and save to new file.
@@ -54,4 +54,3 @@ for wikipage in FOLDER_TO_WIKIDATA.glob('*.txt'):
             # Regex: Take everything between ( and ) with non greedy matching
             text_without_actors = re.sub(r'\(.{1,}?\)', '', complete_text)
             file_to_write.write(text_without_actors)
-            
